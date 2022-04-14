9e23ca151d8133144d499146ac4ea0c459e3690c
saidurraahmaan <saidurraahmaan@gmail.com>
2022-04-14 13:37:30 +0600

    Header Link

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import {Link} from "react-router-dom";
+import ProductListing from "./ProductListing";
 
 const Header = () => {
     return (
         <div className="ui fixed menu">
             <div className="ui container center">
-                <h2>FakeShop</h2>
+                <Link to='/' element={<ProductListing/>}>FakeShop</Link>
+
             </div>
         </div>
     );
